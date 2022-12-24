a4e1af390ce4c82aea4c5ac5ee0daf9b51792652
Archi903 <artpele@yandex.ru>
2022-12-24 14:02:21 +0300

    CourseWork 1

--- src/BookOfEmpleyers/Main.java
+++ src/BookOfEmpleyers/Main.java
@@ -1,60 +1,84 @@
 package BookOfEmpleyers;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class Main {
 
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        CourseWork1();
+    }
+
+    public static void CourseWork1() {
+
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee(" Иванов Иван Иванович", 1, 20000);
+        employee[1] = new Employee(" Петров Илья Григорьевич", 3, 45000);
+        employee[2] = new Employee(" Курчатов Василий Игоревич", 4, 61000);
+        employee[3] = new Employee(" Киселева Марианна Михайловна", 5, 33000);
+        employee[4] = new Employee(" Коновалова Злата Ивановна", 2, 76000);
+        employee[5] = new Employee(" Лобанов Дмитрий Александрович", 4, 65000);
+        employee[6] = new Employee(" Матвеев Роман Ильич", 3, 94000);
+        employee[7] = new Employee(" Кочетов Давид Андреевич", 5, 55000);
+        employee[8] = new Employee(" Кузнецов Фёдор Алексеевич", 1, 72000);
+        employee[9] = new Employee(" Борисов Даниил Иванович", 2, 39000);
 
 
-                        Employee employee1 = new Employee(" Иванов Иван Иванович", 1, 20000);
-                        Employee employee2 = new Employee(" Петров Илья Григорьевич", 3, 45000);
-                        Employee employee3 = new Employee(" Курчатов Василий Игоревич", 4, 61000);
-                        Employee employee4 = new Employee(" Киселева Марианна Михайловна", 5, 33000);
-                        Employee employee5 = new Employee(" Коновалова Злата Ивановна", 2, 76000);
-                        Employee employee6 = new Employee(" Лобанов Дмитрий Александрович", 4, 65000);
-                        Employee employee7 = new Employee(" Матвеев Роман Ильич", 3, 94000);
-                        Employee employee8 = new Employee(" Кочетов Давид Андреевич", 5, 55000);
-                        Employee employee9 = new Employee(" Кузнецов Фёдор Алексеевич", 1, 72000);
-                        Employee employee10 = new Employee(" Борисов Даниил Иванович", 2, 39000);
+        employee[2].setDepartment(2);   // Поменять департамент
+        employee[2].setSalary(47000);   // Поменять зарплату
 
-                        Employee employees[] = {employee1, employee2, employee3, employee4, employee5, employee6, employee7, employee8, employee9, employee10};
+        Arrays.stream(employee).forEach(System.out::println);
 
+        System.out.print("**********************************************************************************");
+        System.out.println("");
 
-                        employee2.setDepartment(2);
-                        employee2.setSalary(47000);
-
-                        Arrays.stream(employees).forEach(System.out::println);
-
-
-                int sum = 0;
-                for (Employee current : employees) {
-                    sum = sum + current.getSalary();
-                }
-                System.out.println("Сумма затрат на зарплаты составила " + sum);
-
-
-                        String minEmployee = null;
-                        int minSum = employees[0].getSalary();
-                        for (int i = 0; i < employees.length; i++) {
-                                if (minSum >= employees[i].getSalary()) {
-                                        minEmployee = employees[i].getName();
-                                        minSum = employees[i].getSalary();
-                                }
-                        }
-                System.out.println("Минимальная зарплата у сотрудника:" + minEmployee + ", зарплата: " + minSum + " рублей");
-
-                        String maxEmployee = null;
-                        int maxSum = 0;
-                        for (int i = 0; i < employees.length; i++) {
-                        if (maxSum <= employees[i].getSalary()) {
-                                maxSum = employees[i].getSalary();
-                                maxEmployee = employees[i].getName();
-                        }
-                }
-                System.out.println("Максимальная зарплата у сотрудника:" + maxEmployee + ", зарплата: " + maxSum + " рублей");
-                }
+        int sumMonth = 0;
+        for (Employee current : employee) {
+            sumMonth = sumMonth + current.getSalary();
         }
+        System.out.println("Сумма затрат на зарплаты составила " + sumMonth);
+
+        System.out.print("**********************************************************************************");
+        System.out.println("");
+
+        String minEmployee = null;
+        int minSum = employee[0].getSalary();
+        for (Employee value : employee) {
+            if (minSum >= value.getSalary()) {
+                minEmployee = value.getName();
+                minSum = value.getSalary();
+            }
+        }
+        System.out.println("Минимальная зарплата у сотрудника:" + minEmployee + ", зарплата: " + minSum + " рублей");
+
+        System.out.print("**********************************************************************************");
+        System.out.println("");
+
+        String maxEmployee = null;
+        int maxSum = employee[0].getSalary();
+        for (Employee value : employee) {
+            if (maxSum <= value.getSalary()) {
+                maxSum = value.getSalary();
+                maxEmployee = value.getName();
+            }
+        }
+        System.out.println("Максимальная зарплата у сотрудника:" + maxEmployee + ", зарплата: " + maxSum + " рублей");
+
+        System.out.print("**********************************************************************************");
+        System.out.println("");
+
+        double averageMonth = (sumMonth * 1.) / employee.length;
+        System.out.println(averageMonth);
+
+        System.out.print("**********************************************************************************");
+        System.out.println("");
+
+        for (int i = 0; i < employee.length; i++) {
+            String employeeName = employee[i].getName();
+            System.out.println("ФИО: " + employeeName);
+        }
+
+    }
+
+}
 
